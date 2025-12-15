df6dc4f5ff5bdb38ab151e3e692c9497c78d0abb
Axel GEREMIA <axel.geremia@enovacom.com>
2025-12-15 11:22:45 +0100

    Ajout tailwind + shadcn ui

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,14 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     host: true, // Permet à Docker d'accéder au port
     port: 5173,
     watch: {
-      usePolling: true //nécessaire sur Windows/WSL pour le hot-reload
-    }
-  }
-})
+      usePolling: true, //nécessaire sur Windows/WSL pour le hot-reload
+    },
+  },
+});
