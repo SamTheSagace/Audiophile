0ccf277a794a298ba706805f573661585e437fbb
mortagh <ax.geremia@gmail.com>
2026-02-02 20:18:39 +0100

    Amelioration du trie par genre

---
+++ back/src/modules/playlists/constants/genre-mappings.constant.ts
@@ -0,0 +1,39 @@
+export const GENRE_FAMILIES_MAPPING: Record<string, string[]> = {
+  'Rock & Metal': [
+    'rock', 'metal', 'punk', 'grunge', 'emo', 'hardcore', 'djent', 'shoegaze', 
+    'crust', 'grindcore', 'pixie', 'noise', 'post-rock', 'new wave', 'rock-and-roll', 'psych'
+  ],
+  'Hip-Hop & Rap': [
+    'rap', 'hip hop', 'trap', 'drill', 'grime', 'boom bap', 'phunk', 'crunk', 
+    'bounce', 'hyphy', 'beatsby', 'lo-fi', 'phonk', 'turntablism'
+  ],
+  'Electro & Dance': [
+    'electro', 'house', 'techno', 'dance', 'edm', 'dubstep', 'dnb', 'drum and bass', 
+    'trance', 'ambient', 'idm', 'breakbeat', 'hardstyle', 'jungle', 'synthpop', 
+    'chiptune', 'glitch', 'industrial', 'ebm', 'chillwave', 'vaporwave'
+  ],
+  'Pop & Variété': [
+    'pop', 'disco', 'k-pop', 'j-pop', 'boy band', 'teen pop', 'schlager', 
+    'chanson', 'europop', 'bubblegum', 'indie pop', 'adult standards'
+  ],
+  'Jazz, Blues & Soul': [
+    'jazz', 'blues', 'soul', 'funk', 'r&b', 'motown', 'gospel', 'swing', 
+    'bebop', 'ska', 'rocksteady', 'rhythm and blues'
+  ],
+  'Classique & Instrumental': [
+    'classical', 'orchestra', 'piano', 'baroque', 'opera', 'contemporary classical', 
+    'choir', 'symphonic', 'chamber', 'early music', 'minimalism'
+  ],
+  'Musiques du Monde & Folk': [
+    'folk', 'reggae', 'salsa', 'cumbia', 'bossa nova', 'samba', 'afrobeat', 
+    'flamenco', 'fado', 'tango', 'country', 'bluegrass', 'americana', 'celtic', 
+    'arabic', 'indian', 'latin', 'mpb', 'highlife', 'kizomba', 'bachata'
+  ],
+  'Soundtrack & Expérimental': [
+    'soundtrack', 'score', 'anime', 'video game music', 'avant-garde', 
+    'experimental', 'field recording', 'noise', 'spoken word', 'poetry'
+  ],
+  'Relaxation & Spirituel': [
+    'meditation', 'new age', 'healing', 'sleep', 'environmental', 'worship', 'christian'
+  ]
+};
